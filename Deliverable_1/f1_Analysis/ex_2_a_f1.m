2ecf8f9d47a77a78ac6e379c6f8b711e0a4cde69
Alexandros Galazoulas <alexandrosgalazoulas96@gmail.com>
2026-02-24 15:27:16 +0200

    Create ex_2_a_f1.m

---
+++ Deliverable_1/f1_Analysis/ex_2_a_f1.m
@@ -0,0 +1,60 @@
+L = [10^-15.65; 0.01; 0.1; 0.5; 1; 2];
+nn = 100;
+g = 0.618;
+
+figure(1);
+clf;
+
+for k = 1:length(L)
+    l = L(k);
+    A = NaN * ones(nn, 1);
+    B = NaN * ones(nn, 1);
+    X = NaN * ones(nn, 2);
+    F1 = NaN * ones(nn, 2);
+
+    A(1, 1) = -1;
+    B(1, 1) = 3;
+    X(1, 1) = A(1, 1) + (1 - g) * (B(1, 1) - A(1, 1));
+    X(1, 2) = A(1, 1) + g * (B(1, 1) - A(1, 1));
+    F1(1, 1) = f1(X(1, 1));
+    F1(1, 2) = f1(X(1, 2));
+
+    for i = 1:nn
+        if (B(i, 1) - A(i, 1)) < l
+            fprintf('Το σημείο ελαχίστου για l = %2.4f βρίσκεται στο διάστημα [%2.4f, %2.4f]\n', l, A(i, 1), B(i, 1));
+            fprintf('Χρειάστηκαν %d επαναλήψεις για να βρεθεί και %d υπολογισμοί της f.\n', i, i + 1);
+            break;
+        elseif f1(X(i, 1)) > f1(X(i, 2))
+            A(i + 1, 1) = X(i, 1);
+            B(i + 1, 1) = B(i, 1);
+            X(i + 1, 2) = A(i + 1, 1) + g * (B(i + 1, 1) - A(i + 1, 1));
+            X(i + 1, 1) = X(i, 2);
+            F1(i + 1, 2) = f1(X(i + 1, 2));
+            F1(i + 1, 1) = F1(i, 2);
+        elseif f1(X(i, 1)) < f1(X(i, 2))
+            A(i + 1, 1) = A(i, 1);
+            B(i + 1, 1) = X(i, 2);
+            X(i + 1, 2) = X(i, 1);
+            X(i + 1, 1) = A(i + 1, 1) + (1 - g) * (B(i + 1, 1) - A(i + 1, 1));
+            F1(i + 1, 1) = f1(X(i + 1, 1));
+            F1(i + 1, 2) = F1(i, 1);
+        end
+
+        fprintf('f1(x1_%d) = f1(%2.4f) = %2.4f και f1(x2_%d) = f1(%2.4f) = %2.4f\n', i, X(i, 1), f1(X(i, 1)), i, X(i, 2), f1(X(i, 2)));
+    end
+
+    X_sorted = sort(X);
+    X_vector = reshape(X_sorted, 1, []);
+    F1_a = NaN * ones(1, length(X_vector));
+    for j = 1:length(X_vector)
+        F1_a(1, j) = f1(X_vector(1, j));
+    end
+    
+    subplot(2, 3, k);
+    plot(X_vector, F1_a);
+    xlabel('X');
+    ylabel('f1(x)');
+    title(sprintf('f1 με l = %g', l));
+end
+
+sgtitle('Απεικόνιση της f1 για διαφορετικές τιμές l με τη μέθοδο του Χρυσού Τομέα');
